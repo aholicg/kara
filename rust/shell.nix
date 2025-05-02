baee97b72bebffba00101338b81d2fe7c24b8884
9r3y <168468366+aholicg@users.noreply.github.com>
2025-05-02 03:57:38 +0200

    Add files via upload

--- rust/shell.nix
+++ rust/shell.nix
@@ -1,1 +1,21 @@
+let
+  # Pinned nixpkgs, deterministic. Last updated: 2/12/21.
+  pkgs = import (fetchTarball("https://github.com/NixOS/nixpkgs/archive/refs/tags/24.11.tar.gz")) {};
 
+  # Rolling updates, not deterministic.
+  # pkgs = import (fetchTarball("channel:nixpkgs-unstable")) {};
+in
+pkgs.callPackage (
+  {
+    mkShell,
+    cargo,
+    rustc,
+  }:
+  mkShell {
+    strictDeps = true;
+    nativeBuildInputs = [
+      cargo
+      rustc
+    ];
+  }
+) { }
